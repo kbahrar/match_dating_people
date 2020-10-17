a247de55765233ba241e02c44c79f996d4c2255e
Youness Benbraitit <ybenbrai@e1r10p5.1337.ma>
2020-10-17 18:20:20 +0100

    added all checks to controllers and middleware for login/firstname/lastname/email

--- server/middleware/register.js
+++ server/middleware/register.js
@@ -1,3 +1,45 @@
+
+function checkNull(body) {
+    console.log(body.password);
+    if (body.password == 0 || body.email == 0 || body.lastName == 0 || body.firstName == 0 || body.login == 0)
+        return false;
+    if (body.password.length == 0 || body.email.length == 0 || body.lastName.length == 0 || body.firstName.length == 0 || body.login.length == 0)
+        return false;
+    return true;
+}
+
+function checkLogin(login) {
+    const loginRegexp = /^[A-Za-z][A-Za-z0-9]{2,31}$/;
+    var regex = loginRegexp.test(login);
+    if (!regex)
+        return false;
+    return true;
+}
+
+function checkFirstName(firstName) {
+    const firstNameRegexp = /^[A-Za-z][A-Za-z]{2,31}$/;
+    var regex = firstNameRegexp.test(firstName);
+    if (!regex)
+        return false;
+    return true;
+}
+
+function checkLastName(lastName) {
+    const lastNameRegexp = /^[A-Za-z][A-Za-z]{2,31}$/;
+    var regex = lastNameRegexp.test(lastName);
+    if (!regex)
+        return false;
+    return true;
+}
+
+function checkMail(email) {
+    const emailRegexp = /^[a-zA-Z0-9\._-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z-]+)*$/;
+    var regex = emailRegexp.test(email);
+    if (!regex)
+        return false;
+    return true;
+}
+
 function checkPwd(pwd) {
     if (pwd.length < 8)
         return false;
@@ -23,25 +65,12 @@ function checkPwd(pwd) {
     return true;
 }
 
-function checkMail(Mail) {
-    const emailRegexp = /^[a-zA-Z0-9\._-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z-]+)*$/;
-    var regex = emailRegexp.test(Mail);
-    if (!regex)
-        return false;
-    return true;
-}
-
-function checkNull(body) {
-    console.log(body.password);
-    if (body.password == 0 || body.email == 0 || body.lastName == 0 || body.firstName == 0 || body.login == 0)
-        return false;
-    if (body.password.length == 0 || body.email.length == 0 || body.lastName.length == 0 || body.firstName.length == 0 || body.login.length == 0)
-        return false;
-    return true;
-}
 
 module.exports = {
     checkPwd: checkPwd,
+    checkLogin: checkLogin,
+    checkFirstName: checkFirstName,
+    checkLastName: checkLastName,
     checkMail: checkMail,
     checkNull: checkNull
 }
